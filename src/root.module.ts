401923c3493fcee99403139634e8f7a0db2fbdcc
Bouteiller <bouteiller.alan@gmail.com>
2019-09-12 17:28:25 +0200

    :card_file_box: feat (env) : add env file
    add db var

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { EnvService } from './env';
 import { HelloModule } from './module/hello.module';
+import { ContentModule } from './module/content.module';
+
+const env = new EnvService().getEnv();
 
 @Module({
     imports: [
         // connect to the mongodb database
-        MongooseModule.forRoot('mongodb://172.11.111.2/blog', { useNewUrlParser: true }),
+        MongooseModule.forRoot(`mongodb://${env.db_user}:${env.db_pass}@${env.db_uri}:${env.db_name}/${env.db_name}`, env.db_option),
         // ping module
         HelloModule,
-        // other module
+        // data module
+        ContentModule,
     ],
 })
 
