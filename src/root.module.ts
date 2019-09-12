2696778e19ccfe993623d3c17500d63dc55acbfa
Bouteiller <bouteiller.alan@gmail.com>
2019-09-12 12:45:13 +0200

    :card_file_box: feat (db) : add mongoose connection module

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { HelloModule } from './module/hello.module';
 
 @Module({
-    imports: [HelloModule],
+    imports: [
+        // connect to the mongodb database
+        MongooseModule.forRoot('mongodb://172.11.111.2/blog', { useNewUrlParser: true }),
+        // ping module
+        HelloModule,
+        // other module
+    ],
 })
 
 export class RootModule {}
