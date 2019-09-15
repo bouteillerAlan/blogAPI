a039b8672b540485db18aa3bbc14138864ec15b3
Alan Bouteiller <bouteiller.alan@gmail.com>
2019-09-15 18:38:47 +0200

    :wrench: refacto (env) : add dev env file

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,19 @@
+// !!!
+// USE THIS FILE IN DEV ONLY
+// REPLACE BY .ENV FILE IN PROD
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EnvService {
     getEnv(): any {
         return {
-            db_uri : '172.11.111.2',
+            db_uri : 'localhost',
             db_port : '27017',
             db_name : 'blog',
-            db_user : 'root',
+            db_user : 'roota',
             db_pass : 'root',
             db_option : { useNewUrlParser: true },
+            bcrypt_salt : 12,
         };
     }
 }
