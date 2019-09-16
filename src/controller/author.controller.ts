bec1951368ac47b5f32846664ed7b7cb4b93bd75
Bouteiller <bouteiller.alan@gmail.com>
2019-09-16 13:52:41 +0200

    :recycle: refacto (validation) : add author validation

--- src/controller/author.controller.ts
+++ src/controller/author.controller.ts
@@ -1,11 +1,18 @@
 import {Body, Controller, Delete, Get, NotFoundException, Param, Post, Put} from '@nestjs/common';
 import { AuthorService } from '../service/author.service';
 import { DtoAuthor } from '../dto/author.dto';
+import { DtoAuthorUpdate } from '../dto/author.update.dto';
 import { isMongoId } from '../function/mongo_id';
 
 @Controller('author')
 export class AuthorController {
     constructor(private readonly authorService: AuthorService) {}
+
+    // check if a name exist
+    private async checkName(name): Promise<any> {
+        const data = await this.authorService.getAuthorByName(name);
+        return data.length === 0;
+    }
 
     @Get(':id')
     async getAuthorById(@Param('id') id): Promise<object> {
@@ -39,37 +46,48 @@ export class AuthorController {
     // !!! here i consider that password is encrypted with bcrypt in front
     @Post('add')
     async setAuthor(@Body() body: DtoAuthor): Promise<object> {
-        const res: any = await this.authorService.setAuthor(body);
-        if (res.errors) {
-            return {status : 'error', message : res.errors};
+        if (this.checkName(name)) {
+            return {status : 'error', message : 'name already exist'};
         } else {
-            return {status : 'ok', message : res};
+            const res: any = await this.authorService.setAuthor(body);
+            if (res.errors) {
+                return {status : 'error', message : res.errors};
+            } else {
+                return {status : 'ok', message : res};
+            }
         }
     }
 
     // !!! this route encrypt the password
     // !!! DO NOT USE IN PROD (cause : man in the middle!)
-    // #todo check if username already exist
     @Post('add/encrypted')
     async setAuthorEncrypted(@Body() body: DtoAuthor): Promise<object> {
-        const res: any = await this.authorService.setAuthorEncrypted(body);
-        if (res.errors) {
-            return {status : 'error', message : res.errors};
+        if (this.checkName(name)) {
+            return {status : 'error', message : 'name already exist'};
         } else {
-            return {status : 'ok', message : res};
+            const res: any = await this.authorService.setAuthorEncrypted(body);
+            if (res.errors) {
+                return {status : 'error', message : res.errors};
+            } else {
+                return {status : 'ok', message : res};
+            }
         }
     }
 
     @Put(':id')
-    updateAuthor(@Param('id') id, @Body() body: DtoAuthor): object {
-        if (!isMongoId(id)) {
-            throw new NotFoundException('This id dosen\'t exist.');
+    async updateAuthor(@Param('id') id, @Body() body: DtoAuthorUpdate): Promise<any> {
+        if (this.checkName(name)) {
+            return {status : 'error', message : 'name already exist'};
+        } else {
+            if (!isMongoId(id)) {
+                throw new NotFoundException('This id dosen\'t exist.');
+            }
+            return await this.authorService.updateAuthor(id, body);
         }
-        return this.authorService.updateAuthor(id, body);
     }
 
     @Delete(':id')
-    async deleteAuthor(@Param('id') id): Promise<object> {
+    async deleteAuthor(@Param('id') id): Promise<any> {
         if (!isMongoId(id)) {
             throw new NotFoundException('This id dosen\'t exist.');
         }
