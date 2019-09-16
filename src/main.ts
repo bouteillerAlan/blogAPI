02367c96ac3006a49c9d8402cfdda2bd47ef8a6c
Bouteiller <bouteiller.alan@gmail.com>
2019-09-16 13:50:50 +0200

    🚧 feat (validation) : add validation in server settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { RootModule } from './root.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
+
+  // Use global validation pipes, for all requests using DTO.
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+
   await app.listen(3000);
 }
 bootstrap();
