500b47d7240f0b28cd0003d63f128f9e6e69c64f
Bouteiller <bouteiller.alan@gmail.com>
2019-09-19 16:44:49 +0200

    🚧 feat (auth) : add jwt generation

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,26 +1,48 @@
 import {Injectable} from '@nestjs/common';
 import {AuthorService} from './author.service';
 import * as bcrypt from 'bcrypt';
-import {EnvService} from '../env';
-
-const env = new EnvService().getEnv();
+import {Model} from 'mongoose';
+import {JwtService} from '@nestjs/jwt';
+import {InjectModel} from '@nestjs/mongoose';
+import {AuthorInterface} from '../interface/author.interface';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly authorService: AuthorService) {}
+    constructor(
+        @InjectModel('Author') private readonly authorModel: Model<AuthorInterface>,
+        private readonly authorService: AuthorService,
+        private readonly jwtService: JwtService,
+    ) {}
 
-    async validateUser(name: string, password: string): Promise<any> {
-        const user: any = await this.authorService.getAuthorByName(name);
-        if (user) {
-            bcrypt.compare(password, user.password, (err, res) => {
-                if (!err) {
-                    return {status: 'ok', message: res};
-                } else {
-                    return {status: 'error', message: err};
-                }
-            });
+    // check if user exist in db, if is return user
+    async validateUser(name: string): Promise<any> {
+        const user = await this.authorService.getAuthorByName(name);
+        if (user.length !== 0) {
+            return {status: true, data: user[0]};
         } else {
-            return {status: 'error', message: user};
+            return {status: false, data: 'no user with this name'};
+        }
+    }
+
+    // check user and return token if user is valid
+    async login(author: any) {
+        const check = await this.validateUser(author.name);
+        if (check.status) {
+            const compare = await bcrypt.compare(author.password, check.data.password);
+            if (!compare) {
+                // if check is not ok
+                return {status: false, message: 'bad credential'};
+            } else {
+                // generate jwt payload
+                const payload = {name: author.name, sub: author._id};
+                const jwt = this.jwtService.sign(payload);
+                // save in db
+                await this.authorModel.updateOne({_id: check.data._id}, {$set: {token: jwt}});
+                // return
+                return {access_token: jwt};
+            }
+        } else {
+            return {status: false, message: 'no user found'};
         }
     }
 }
