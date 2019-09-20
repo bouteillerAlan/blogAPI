e114cc5e8b487be2e1998df37c405979ab7def30
Bouteiller <bouteiller.alan@gmail.com>
2019-09-20 15:57:33 +0200

    :construction: feat (auth) : add test route

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -6,15 +6,18 @@ import {AuthService} from '../service/auth.service';
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    // give the jwt
     @Post()
     async login(@Request() req): Promise<any> {
         return await this.authService.login(req.body);
     }
 
+    // just for test, return the user
     @UseGuards(AuthGuard('jwt'))
     @Get('validate')
     async validate(@Request() req): Promise<any> {
-        return 'ok';
+        // req.user is predefined
+        return req.user;
     }
 
 }
