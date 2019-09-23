e752c4429975020bd566ec98e3b5740f0fa92dd8
Bouteiller <bouteiller.alan@gmail.com>
2019-09-23 15:33:33 +0200

    :construction: refactor (mailer) : improve response for checkData

--- src/function/checkData.ts
+++ src/function/checkData.ts
@@ -1,15 +1,17 @@
 // return true if data is ok
-export function CheckData(stateName, value, minL, maxL, regexPattern, errorRegex, mail= false) {
+export function CheckData(value, minL, maxL, regexPattern, errorRegex, mail= false) {
     // build regex
     const regex = new RegExp(regexPattern, 'gm')
     // check
     if (regex.exec(value)) {
-        return false;
+        return {status: false, cause: 'regex'};
     } else if (value.length < minL) {
-        return false;
+        return {status: false, cause: 'minL'};
     } else if (value.length > maxL) {
-        return false;
+        return {status: false, cause: 'maxL'};
+    } else if ((mail && !value.match(/[\S]+[@][a-z]+[.][a-z]+/gm))) {
+        return {status: false, cause: 'notMail'};
     } else {
-        return !(mail && !value.match(/[\S]+[@][a-z]+[.][a-z]+/gm));
+        return {status: true};
     }
 }
