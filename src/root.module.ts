54636934fb49043e055bea7e514c4420a212748c
Bouteiller <bouteiller.alan@gmail.com>
2019-09-23 15:32:32 +0200

    :construction: feat (mailer) : add templating in module

--- src/root.module.ts
+++ src/root.module.ts
@@ -7,6 +7,8 @@ import { AuthorModule } from './module/author.module';
 import { AuthModule } from './module/auth.module';
 import { ConfigService } from './conf/config.service';
 import { ConfigModule } from './conf/config.module';
+import {PugAdapter, MailerModule} from '@nest-modules/mailer';
+import {MailModule} from './module/mail.module';
 
 @Module({
     imports: [
@@ -19,6 +21,25 @@ import { ConfigModule } from './conf/config.module';
             }),
             inject: [ConfigService],
         }),
+        // mailer
+        MailerModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (env: ConfigService) => ({
+                transport: `smtps://${env.get('smtp_user')}:${env.get('smtp_pass')}@${env.get('smtp_host')}`,
+                defaults: {
+                    from: `"${env.get('smtp_user_from')}" <${env.get('smtp_mail_from')}>`,
+                },
+                template: {
+                    dir: __dirname + env.get('smtp_dirname'),
+                    adapter: new PugAdapter(), // or new PugAdapter()
+                    options: {
+                        strict: true,
+                    },
+                },
+            }),
+            inject: [ConfigService],
+        }),
+        MailModule,
         // ping module
         HelloModule,
         // auth module
